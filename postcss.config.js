511703b27f7d54f8b3506fa9a3eaac7740c225c4
Ricy137 <1932353948@qq.com>
2022-12-23 22:19:06 +0800

    feat: replace webpack with vite

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    'postcss-px-to-viewport': {
-      viewportWidth: 1800,
-    }
-  },
-}
---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import Unocss from 'unocss/vite';
+import presetIcons from '@unocss/preset-icons';
+import presetWind from '@unocss/preset-wind';
+import transformerDirective from '@unocss/transformer-directives';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+export default defineConfig({
+  server: {
+    proxy: {},
+  },
+  plugins: [
+    Unocss({
+      transformers: [transformerDirective()],
+      presets: [
+        presetWind(),
+        presetIcons({
+          prefix: 'i-',
+          extraProperties: {
+            display: 'inline-block',
+            'vertical-align': 'middle',
+          },
+        }),
+      ],
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@modules': path.resolve(__dirname, 'src/modules'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@router': path.resolve(__dirname, 'src/router'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@service': path.resolve(__dirname, 'src/service'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [visualizer()],
+    },
+  },
+});
