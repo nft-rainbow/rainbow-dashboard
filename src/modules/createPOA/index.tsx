4e54f1cb73ff07207ce4cb9f07269106a7ff8255
Ricy137 <1932353948@qq.com>
2022-12-29 00:55:18 +0800

    feat: rechange activity

--- src/modules/createPOA/index.tsx
+++ src/modules/createPOA/index.tsx
@@ -10,11 +10,22 @@ import {
   Col,
   Switch,
   DatePicker,
+  Select,
+  Popover
 } from "antd";
+import { QuestionCircleTwoTone } from '@ant-design/icons';
 import FileUpload from '../../components/FileUpload';
+
+const Content = (
+  <div>
+    开启后，只允许导入的地址进行铸造，并且只允许铸造一定数量的藏品，例如地址为A,B,C，填写数量为1,2,3，则意味着A允许铸造1个藏品，B铸造2个，C铸造3个。注意使用英文逗号分隔。关闭即开放给所有地址进行铸造。
+  </div>
+)
 
 const CreatePOA: React.FC<ModalProps & ModalFuncProps & FormProps> = ({ open, onOk, onCancel, onFinish }) => {
   const [form] = Form.useForm();
+  const { Option } = Select;
+  const { RangePicker } = DatePicker;
   const [dateDisabled, setDateDisabled] = useState(false);
   const [numberDisabled, setNumberDisabled] = useState(false);
   const [proofDisabled, setProofDisabled] = useState(false);
@@ -31,91 +42,82 @@ const CreatePOA: React.FC<ModalProps & ModalFuncProps & FormProps> = ({ open, on
         onFinishFailed={onCancel}
         autoComplete="off"
       >
-        <Col span={16}>
-          <Form.Item
-            label="合约地址"
-            name="contractAddress"
-            rules={[{ required: true, message: '请输入合约地址' }]}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={16}>
-          <Form.Item
-            label="活动名称"
-            name="ActivityName"
-            rules={[{ required: true, message: '请输入活动名称' }]}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={16}>
-          <Form.Item
-            label="活动描述"
-            name="ActivityDescription"
-            rules={[{ required: true, message: '请输入活动描述' }]}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Row>
-          <Col span={10}>
-            <Form.Item
-              label="开始时间"
-              name="startDate"
-              rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-              <DatePicker />
-            </Form.Item>
-          </Col>
-          <Col span={10} offset={1}>
-            <Form.Item
-              label="结束时间"
-              name="endDate"
-            >
-              <DatePicker disabled={dateDisabled} />
-            </Form.Item>
-          </Col>
-          <Col span={1} offset={1}>
-            <Switch onChange={(check) => { setDateDisabled(check) }} />
-          </Col>
-        </Row>
-        <Col span={16} >
-          <Form.Item
-            label="图片"
-            name="picture"
-            rules={[{ required: true, message: '请上传图片' }]}
-          >
-            <FileUpload onChange={(err: Error, file: any) => form.setFieldsValue({ file_url: file.url })} />
-          </Form.Item>
-        </Col>
-        <Row>
-          <Col span={11}>
-            <Form.Item
-              label="发行数量"
-              name='issueNumber'
-            >
-              <Input disabled={numberDisabled} />
-            </Form.Item>
-          </Col>
-          <Col span={11} offset={1}>
-            <Switch onChange={(checked) => { setNumberDisabled(checked) }} />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={11} >
-            <Form.Item
-              label="领取口令"
-              name="proof"
-            >
-              {proofDisabled && <Input />}
-            </Form.Item>
+        <Form.Item name="chain" label="网络" rules={[{ required: true }]}>
+          <Select>
+            <Option value="conflux">树图主网</Option>
+            <Option value="conflux_test">树图测试网</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="合约地址"
+          name="contractAddress"
+          rules={[{ required: true, message: '请输入合约地址' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="活动名称"
+          name="ActivityName"
+          rules={[{ required: true, message: '请输入活动名称' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="活动描述"
+          name="ActivityDescription"
+          rules={[{ required: true, message: '请输入活动描述' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <RangePicker />
+        </Form.Item>
+        {/* <Form.Item
+          label="开始时间"
+          name="startDate"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <DatePicker />
+        </Form.Item>
+        <Form.Item
+          label="结束时间"
+          name="endDate"
+        >
+          <DatePicker disabled={dateDisabled} />
+        </Form.Item> */}
+        <Switch onChange={(check) => { setDateDisabled(check) }} />
 
-          </Col>
-          <Col span={11} offset={1}>
-            <Switch onChange={(checked) => { setProofDisabled(checked) }} />
-          </Col>
-        </Row>
+        <Form.Item
+          label="图片"
+          name="picture"
+          rules={[{ required: true, message: '请上传图片' }]}
+        >
+          <FileUpload onChange={(err: Error, file: any) => form.setFieldsValue({ file_url: file.url })} />
+        </Form.Item>
+        <Form.Item
+          label="发行数量"
+          name='issueNumber'
+        >
+          <Input disabled={numberDisabled} />
+        </Form.Item>
+        <Switch onChange={(checked) => { setNumberDisabled(checked) }} />
+        <Form.Item
+          label="领取口令"
+          name="proof"
+        >
+          {proofDisabled && <Input />}
+        </Form.Item>
+        <Switch onChange={(checked) => { setProofDisabled(checked) }} />
+        <Form.Item
+          label="白名单铸造："
+          name="whitelist"
+        >
+          {proofDisabled && <Input />}
+        </Form.Item>
+        <Popover content={Content} title="whitelist">
+          <QuestionCircleTwoTone />
+        </Popover>
+        <Switch onChange={(checked) => { setProofDisabled(checked) }} />
       </Form>
     </Modal >
   )
