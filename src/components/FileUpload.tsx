71470d9493a6d5cb4e3f6a6e5414b89c69881e0e
Ricy <1932353948@qq.com>
2023-01-28 18:55:53 +0800

    feat: create activity

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,25 +1,28 @@
 import { Upload, Button } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { UploadOutlined, PlusOutlined } from '@ant-design/icons';
 import { methodUrl, authHeaderSync } from '../services';
 
 export default function FileUpload(props: any) {
-    const action = props.private ? methodUrl("/dashboard/misc/upload/kyc") : methodUrl("/dashboard/misc/upload");
-    const uploadProps = {
-        name: 'file',
-        action,
-        headers: authHeaderSync(),
-        onChange(info: any) {
-            if (info.file.status === 'done') {
-                const data = info.file.response;
-                props.onChange(null, data);
-            } else if (info.file.status === 'error') {
-                props.onChange(new Error('Upload failed.'));
-            }
-        },
-    };
-    return (
-        <Upload {...uploadProps}>
-            <Button icon={<UploadOutlined />}>上传</Button>
-        </Upload>
-    )
+  console.log('file', props);
+  const action = props.private ? methodUrl('/dashboard/misc/upload/kyc') : methodUrl('/dashboard/misc/upload');
+  const uploadProps = {
+    name: 'file',
+    action,
+    headers: authHeaderSync(),
+    onChange(info: any) {
+      if (info.file.status === 'done') {
+        const data = info.file.response;
+        props.onChange(null, data);
+      } else if (info.file.status === 'error') {
+        props.onChange(new Error('Upload failed.'));
+      }
+    },
+  };
+  return (
+    <Upload {...uploadProps}>
+      <Button className={props.className ?? ''} icon={!!props.type && props.type == 'plus' ? <PlusOutlined /> : <UploadOutlined />}>
+        {!!props.type && props.type == 'plus' ? '' : '上传'}
+      </Button>
+    </Upload>
+  );
 }
--- src/modules/createPOA/index.tsx
+++ src/modules/createPOA/index.tsx
@@ -1,18 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Modal,
-  ModalProps,
-  ModalFuncProps,
-  Form,
-  FormProps,
-  Input,
-  Row,
-  Col,
-  Switch,
-  DatePicker,
-  Select,
-  Popover
-} from "antd";
+import { Modal, ModalProps, ModalFuncProps, Form, FormProps, Input, Row, Col, Switch, DatePicker, Select, Popover } from 'antd';
 import { QuestionCircleTwoTone } from '@ant-design/icons';
 import FileUpload from '../../components/FileUpload';
 
@@ -20,7 +7,7 @@ const Content = (
   <div>
     开启后，只允许导入的地址进行铸造，并且只允许铸造一定数量的藏品，例如地址为A,B,C，填写数量为1,2,3，则意味着A允许铸造1个藏品，B铸造2个，C铸造3个。注意使用英文逗号分隔。关闭即开放给所有地址进行铸造。
   </div>
-)
+);
 
 const CreatePOA: React.FC<ModalProps & ModalFuncProps & FormProps> = ({ open, onOk, onCancel, onFinish }) => {
   const [form] = Form.useForm();
@@ -31,96 +18,72 @@ const CreatePOA: React.FC<ModalProps & ModalFuncProps & FormProps> = ({ open, on
   const [proofDisabled, setProofDisabled] = useState(false);
 
   return (
-    <Modal title="创建活动" open={open} onOk={form.submit} onCancel={onCancel}>
-      <Form
-        name="basic"
-        form={form}
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 18 }}
-        onFinish={onFinish}
-        initialValues={{ chain: 'conflux' }}
-        onFinishFailed={onCancel}
-        autoComplete="off"
-      >
-        <Form.Item name="chain" label="网络" rules={[{ required: true }]}>
+    <Modal title="创建活动" open={open} onOk={form.submit} onCancel={onCancel} width={'30.5%'} style={{ top: '14px' }} bodyStyle={{ paddingTop: '16px' }}>
+      <Form name="basic" form={form} layout="vertical" onFinish={onFinish} initialValues={{ chain: 'conflux' }} onFinishFailed={onCancel} autoComplete="off">
+        <Form.Item name="chain" label="区块链" rules={[{ required: true }]}>
           <Select>
-            <Option value="conflux">树图主网</Option>
-            <Option value="conflux_test">树图测试网</Option>
+            <Option value="conflux">树图链</Option>
+            <Option value="conflux_test">树测试链</Option>
           </Select>
         </Form.Item>
-        <Form.Item
-          label="合约地址"
-          name="contractAddress"
-          rules={[{ required: true, message: '请输入合约地址' }]}
-        >
+        <Form.Item label="合约地址" name="contractAddress" rules={[{ required: true, message: '请输入合约地址' }]}>
           <Input />
         </Form.Item>
-        <Form.Item
-          label="活动名称"
-          name="ActivityName"
-          rules={[{ required: true, message: '请输入活动名称' }]}
-        >
+        <Form.Item label="活动名称" name="ActivityName" rules={[{ required: true, message: '请输入活动名称' }]}>
           <Input />
         </Form.Item>
-        <Form.Item
-          label="活动描述"
-          name="ActivityDescription"
-          rules={[{ required: true, message: '请输入活动描述' }]}
-        >
+        <Form.Item label="活动描述" name="ActivityDescription" rules={[{ required: true, message: '请输入活动描述' }]}>
           <Input />
         </Form.Item>
-        <Form.Item>
-          <RangePicker />
+        <div className="mb-8px flex flex-row justify-between">
+          <label htmlFor="activityDate">活动日期：</label>
+          <div>
+            <Switch
+              onClick={(checked, e) => {
+                e.preventDefault();
+                setDateDisabled(checked);
+              }}
+            />
+            <span className="ml-8px">不限制结束日期</span>
+          </div>
+        </div>
+        <Form.Item id="activityDate">
+          <RangePicker id="activityDate" showTime disabled={[false, dateDisabled]} />
         </Form.Item>
-        {/* <Form.Item
-          label="开始时间"
-          name="startDate"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <DatePicker />
+        <Form.Item label="上传图片：" rules={[{ required: true, message: '请上传图片' }]}>
+          <FileUpload onChange={(err: Error, file: any) => form.setFieldsValue({ file_url: file.url })} type="plus" />
+          <div className='mt-8px'>支持上传PNG、GIF、SVG、JPG、视频等格式，大小限制 5MB，推荐 1:1比例，如果图片是圆形，建议圆形图案正好在中间</div>
         </Form.Item>
-        <Form.Item
-          label="结束时间"
-          name="endDate"
-        >
-          <DatePicker disabled={dateDisabled} />
-        </Form.Item> */}
-        <Switch onChange={(check) => { setDateDisabled(check) }} />
-
-        <Form.Item
-          label="图片"
-          name="picture"
-          rules={[{ required: true, message: '请上传图片' }]}
-        >
-          <FileUpload onChange={(err: Error, file: any) => form.setFieldsValue({ file_url: file.url })} />
-        </Form.Item>
-        <Form.Item
-          label="发行数量"
-          name='issueNumber'
-        >
+        <Form.Item label="发行数量" name="issueNumber">
           <Input disabled={numberDisabled} />
         </Form.Item>
-        <Switch onChange={(checked) => { setNumberDisabled(checked) }} />
-        <Form.Item
-          label="领取口令"
-          name="proof"
-        >
+        <Switch
+          onChange={(checked) => {
+            setNumberDisabled(checked);
+          }}
+        />
+        <Form.Item label="领取口令" name="proof">
           {proofDisabled && <Input />}
         </Form.Item>
-        <Switch onChange={(checked) => { setProofDisabled(checked) }} />
-        <Form.Item
-          label="白名单铸造："
-          name="whitelist"
-        >
+        <Switch
+          onChange={(checked) => {
+            setProofDisabled(checked);
+          }}
+        />
+        <Form.Item label="白名单铸造：" name="whitelist">
           {proofDisabled && <Input />}
         </Form.Item>
         <Popover content={Content} title="whitelist">
           <QuestionCircleTwoTone />
         </Popover>
-        <Switch onChange={(checked) => { setProofDisabled(checked) }} />
+        <Switch
+          onChange={(checked) => {
+            setProofDisabled(checked);
+          }}
+        />
       </Form>
-    </Modal >
-  )
-}
+    </Modal>
+  );
+};
 
 export default CreatePOA;
