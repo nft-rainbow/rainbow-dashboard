962f330580a595a9739f0da5ab3dd945a45ca15d
Ricy <1932353948@qq.com>
2023-02-14 17:18:02 +0800

    feat: manage assets

---
+++ src/components/PictureReminder.tsx
@@ -0,0 +1,7 @@
+const PictureReminder: React.FC = () => (
+  <div className="mt-8px mb-24px text-12px font-400 text-#9B99A5 leading-17px">
+    支持上传PNG、GIF、SVG、JPG、视频等格式，大小限制 5MB，推荐 1:1比例，如果图片是圆形，建议圆形图案正好在中间
+  </div>
+);
+
+export default PictureReminder;
--- src/pages/activities/manageAssets/index.tsx
+++ src/pages/activities/manageAssets/index.tsx
@@ -1,6 +1,83 @@
-import { Card } from 'antd';
+import { Card, Button, Form, Input, Dropdown, type MenuProps } from 'antd';
+import { DownOutlined, DeleteOutlined } from '@ant-design/icons';
+import FileUpload from '@components/FileUpload';
+import PictureReminder from '@components/PictureReminder';
+const items: MenuProps['items'] = [
+  {
+    label: '文本',
+    key: 'text',
+  },
+  {
+    label: '日期',
+    key: 'date',
+  },
+];
+interface CharacterItemProps {
+  type: string;
+  name: number;
+  remove: (index: number | number[]) => void;
+}
+const CharacterItem: React.FC<CharacterItemProps> = ({ type, name, remove }) => {
+  return (
+    <>
+      <Form.Item dependencies={[['type']]} noStyle name={[name, 'item']}>
+        <>
+          {type === 'text' && <Input placeholder="请输入" />}
+          {type === 'date' && <Input placeholder="请选择" />}
+        </>
+      </Form.Item>
+      <Button type="primary" icon={<DeleteOutlined />} onClick={() => remove([name])} />
+    </>
+  );
+};
+
 const Asset: React.FC = () => {
-  return <Card>123</Card>;
+  const [form] = Form.useForm();
+  const { getFieldValue } = form;
+  return (
+    <Card>
+      <Form id="manageAssetsForm" name="basic" form={form} layout="vertical">
+        <Form.Item label="上传图片：" name="image_url" rules={[{ required: true, message: '请上传图片' }]} className="mb-0">
+          <FileUpload
+            onChange={(err: Error, file: any) => {
+              form.setFieldsValue({ image_url: file.url });
+            }}
+            type="plus"
+            wrapperClass="block w-full"
+            className="block"
+          />
+        </Form.Item>
+        <PictureReminder />
+        <div className="grid grid-cols-2 gap-x-16px">
+          <Form.Item name="name" label="藏品名称：" rules={[{ required: true, message: '请输入藏品名称' }]}>
+            <Input placeholder="2023烤仔守护神兔全家福" />
+          </Form.Item>
+          <Form.Item name="contract_address" label="合约地址" rules={[{ required: true, message: '请选择合约地址' }]}>
+            <Input placeholder="请选择" />
+          </Form.Item>
+        </div>
+        <Form.Item label="特征设置：">
+          <Form.List name="characters">
+            {(fields, { add, remove }) => (
+              <>
+                <Dropdown trigger={['click']} menu={{ items, onClick: (e) => add({ type: e.key }) }} className="!w-64px">
+                  <div>
+                    新增
+                    <DownOutlined />
+                  </div>
+                </Dropdown>
+                <div className="grid grid-cols-2 gap-x-16px">
+                  {fields.map((field, index) => (
+                    <CharacterItem type={getFieldValue(['characters', index, 'type'])} key={field.key} name={field.name} remove={remove} />
+                  ))}
+                </div>
+              </>
+            )}
+          </Form.List>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 };
 
 export default Asset;
