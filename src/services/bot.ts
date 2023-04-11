c33af5654360268f6a8f6ae542e7374be301fdbc
PanaW <jiuhuadao@proton.me>
2023-04-11 10:54:58 +0800

    misc

--- src/services/bot.ts
+++ src/services/bot.ts
@@ -1,26 +1,58 @@
 import { get, post } from '.';
 
 // bot 1-discord 2-dodo
-export async function getBotEvents(bot: string, page: number, limit: number) {
-    return get(`/apps/poap/activity/push/${bot}?page=${page}&limit=${limit}`);
+export async function getBotEvents(bot = '2', page: number, limit: number) {
+    return get(`/apps/botserver`, {
+        social_tool: bot || '2',
+        page,
+        limit,
+    });
 }
 
-export async function getDodoServers(bot: string) {
-    return get(`/apps/poap/activity/servers/${bot}`);
+export async function getDodoServers(bot = '2') {
+    return get(`/apps/botserver`, {
+        social_tool: bot
+    });
 }
 
-export async function getDodoChannels(server_id: string) {
-    return get(`/apps/poap/activity/channels/dodo/${server_id}`);
+export async function getDodoChannels(server_id: string, bot = '2') {
+    return get(`/apps/botserver/channels`, {
+        server_id,
+        social_tool: bot
+    });
 }
 
-export async function getDoDoRoles(server_id: string) {
-    return get(`/apps/poap/activity/roles/dodo/${server_id}`);
+export async function getDoDoRoles(server_id: string, bot = '2') {
+    return get(`/apps/botserver/roles`, {
+        server_id,
+        social_tool: bot
+    });
 }
 
+/**
+{
+    "activity_id": 1,
+    "channel_id": "1",
+    "roles": "1",
+    "content": "content",
+    "color_theme": "red"
+}
+*/
 export async function createBotEvents(meta: object) {
-    return post('/apps/poap/activity/push', meta);
+    return post('/apps/botserver/push', meta);
 }
 
-export async function requestBotValidateCode(server_id: string) {
-    return post('/apps/poap/activity/validate', { server_id });
+export async function getDodoAuthCode(server_id: string, bot: string) {
+    return post('/apps/botserver/authcode', {
+        server_id,
+        social_tool: bot,
+    });
+}
+
+export async function bindDodoServer(server_id: string, auth_code: string, social_tool: string) {
+    return post('/apps/botserver', {
+        server_id,
+        auth_code,
+        social_tool,
+    });
 }
