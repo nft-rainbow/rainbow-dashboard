62a98f741cbafaeb30152f9d513c039486d42639
PanaW <jiuhuadao@proton.me>
2023-04-24 14:43:31 +0800

    bot api inter

--- src/pages/bots/index.tsx
+++ src/pages/bots/index.tsx
@@ -2,18 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { 
     Tabs, TabsProps, Card, Form, Input, 
     Button, Space, Table, Layout, Modal,
-    Row, Col, Divider, Select, Checkbox,
-    Alert, Typography
+    Row, Col, Divider, Select, 
+    Alert, Typography, TablePaginationConfig,
+    message,
 } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { BotEvent } from "@models/index";
-import { getBotEvents, getDoDoRoles, getDodoChannels, getDodoServers } from '@services/bot';
-import { getActivities, ActivityQuerier } from '@services/activity';
+import { BotServer, BotEvent, BotChannel, BotRoles, ActivityItem } from "@models/index";
+import { 
+    getBotInviteUrl, getBotAuthCode, bindBotServers,
+    getBotServers, getServerChannels, getServerRoles,
+    getBotActivities, pushBotActivity, createPushInfo,
+
+} from '@services/bot';
+import { getActivities } from '@services/activity';
+import { short, formatDate } from '@utils/index';
 const { Content, Sider } = Layout;
 const { Text } = Typography;
-
-const DISCORD_BOT = 'discord';
-const DODO_BOT = 'dodo';
+const { Option } = Select;
 
 export default function Page() {
 
@@ -44,23 +49,16 @@ export default function Page() {
 }
 
 function Dodo() {
-    const [form] = Form.useForm();
-    const [isModalShow, setIsModalShow] = useState(false);
-    const [serverId, setServerId] = useState("123");
-    
-    const [isAddBotShow, setIsAddBotShow] = useState(true);
+    const social_tool = 'dodo';
 
-    const showModal = () => {
-        setIsModalShow(true);
-    };
+    const [isModalShow, setIsModalShow] = useState(false);
     
-    const handleOk = () => {
-        setIsModalShow(false);
-    };
+    const [form] = Form.useForm();
     
-    const handleCancel = () => {
-        setIsModalShow(false);
-    };
+    const [isAddBotShow, setIsAddBotShow] = useState(false);
+    const [inviteUrl, setInviteUrl] = useState("");
+    const [bindForm] = Form.useForm();
+    const [serverId, setServerId] = useState(""); // server id in bind form
 
     const onFinish = (values: any) => {
         console.log('Finish:', values);
@@ -70,53 +68,43 @@ function Dodo() {
         {
           title: '互动/藏品ID',
           dataIndex: 'event_id',
-          key: 'event_id',
           render: (text) => <a>{text}</a>,
         },
         {
           title: '类型',
           dataIndex: 'type',
-          key: 'type',
         },
         {
           title: '社群ID',
           dataIndex: 'server_id',
-          key: 'server_id',
         },
         {
           title: '社群名称',
           dataIndex: 'server_name',
-          key: 'server_name',
         },
         {
           title: '互动/藏品名称',
           dataIndex: 'event_name',
-          key: 'event_name',
         },
         {
           title: '区块链',
           dataIndex: 'chain',
-          key: 'chain',
         },
         {
           title: '合约地址',
           dataIndex: 'contract',
-          key: 'contract',
         },
         {
           title: '开始时间',
           dataIndex: 'start_time',
-          key: 'start_time',
         },
         {
           title: '结束时间',
           dataIndex: 'end_time',
-          key: 'end_time',
         },
         {
           title: '推送时间',
           dataIndex: 'push_time',
-          key: 'push_time',
         },
         /* {
           title: 'Action',
@@ -129,7 +117,7 @@ function Dodo() {
           ),
         }, */
     ];
-      
+
     const data: BotEvent[] = [
         {
             id: 1,
@@ -148,33 +136,25 @@ function Dodo() {
         },
     ];
 
+    const sendAuthCode = async (server_id: string) => {
+        const res = await getBotAuthCode(server_id, social_tool);
+    }
+
+    const bindServer = async (values: any) => {
+        const res = await bindBotServers(values.code, values.server_no, social_tool);
+        setIsAddBotShow(false);
+    }
+
     useEffect(() => {
-        getBotEvents(DODO_BOT, 1, 10).then((res) => {
-            console.log('bot events', res);
+        if (!isAddBotShow) return;
+        getBotInviteUrl(social_tool).then(res => {
+            setInviteUrl(res.message);
         })
-    }, []);
+    }, [isAddBotShow]);
 
     useEffect(() => {
-        getDodoServers(DODO_BOT).then((res) => {
-            console.log('dodo servers', res);
-        })
-    }, []);
-
-    /* useEffect(() => {
-        getDoDoRoles(serverId).then((res) => {
-            console.log('dodo roles', res);
-        });
-    }, [serverId]); */
-
-    /* useEffect(() => {
-        getDodoChannels(serverId).then(res => {
-            console.log('dodo channels', res);
-        });
-    }); */
-
-    useEffect(() => {
-        getActivities({page: 1, limit: 10}).then(res => {
-            console.log('activities', res);
+        getBotActivities({social_tool}).then(res => {
+            console.log('bot activities', res);
         });
     });
 
@@ -193,78 +173,27 @@ function Dodo() {
             <Sider theme='light' width={300}>
                 <Space>
                     <Button type='primary' onClick={() => setIsAddBotShow(true)}>添加机器人</Button>
-                    <Button type='primary' onClick={showModal}>推送社群</Button>
+                    <Button type='primary' onClick={() => setIsModalShow(true)}>推送社群</Button>
                 </Space>
             </Sider>
         </Layout>
         <div style={{height: '20px'}}></div>
         <Table rowKey={'event_id'} columns={columns} dataSource={data} />
-        <Modal 
-            title="推送设置" 
-            open={isModalShow} 
-            onOk={handleOk} 
-            onCancel={handleCancel} 
-            width={800}
-            okText={"确定"}
-            cancelText={"取消"}
-        >
-            <Alert type='info' message='可以将活动推送到授权的 DoDo 或 Discord 社区频道中，提供给社群成员进行藏品铸造，活动需先在管理后台中创建好'/>
-            <Form className='mt-20'>
-                <Row>
-                    <Col span={11}>
-                        <Form.Item label="推送服务器" name="server_id">
-                            <Select></Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={11} offset={1}>
-                        <Form.Item label="身份组" name="role_group">
-                            <Select></Select>
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={11}>
-                        <Form.Item label="推送频道" name="channel">
-                            <Select></Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={11} offset={1}>
-                        <Checkbox>单个账户铸造限制</Checkbox>
-                        <Input style={{width: "100px"}} />
-                    </Col>
-                </Row>
-                <Divider/>
-                <h4>设置推送内容</h4>
-                <Row>
-                    <Col span={11}>
-                        <Form.Item label="消息内容" name="content">
-                            <Input placeholder="推送消息内容" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={11} offset={1}>
-                        {/* <Form.Item label="社群ID" name="server_id">
-                            <Input placeholder="Server ID" />
-                        </Form.Item> */}
-                    </Col>
-                </Row>
-                <Divider/>
-                <h4>选择推送消息</h4>
-            </Form>
-        </Modal>
+        <DoDoActivityCreateModal isModalShow={isModalShow} setIsModalShow={setIsModalShow} />
         <Modal
             title="添加机器人"
             open={isAddBotShow}
-            onOk={() => setIsAddBotShow(false)}
+            onOk={() => bindForm.submit()}
             onCancel={() => setIsAddBotShow(false)}
             okText={"确定"}
             cancelText={"取消"}
         >
-            <Form layout='vertical' className='mt-20'>
+            <Form form={bindForm} layout='vertical' className='mt-20' onFinish={bindServer}>
                 <Form.Item>
-                    <Text>1. 点击机器人<a href="https://imdodo.com/bot/u/3574817" target={"_blank"} rel="noreferrer">邀请链接</a>，授权机器人加入自己创建的 DoDo 社区</Text>
+                    <Text>1. 点击机器人<a href={inviteUrl} target={"_blank"} rel="noreferrer">邀请链接</a>，授权机器人加入自己创建的 DoDo 社区</Text>
                 </Form.Item>
                 <Form.Item label="2. 填写自己的超级群号" name="server_no">
-                    <Input placeholder="输入超级群号" />
+                    <Input placeholder="输入超级群号" onChange={e => setServerId(e.target.value)} />
                 </Form.Item>
                 <Form.Item label="3. 填写机器人私信的验证码，完成验证" name="code">
                     <Row gutter={8}>
@@ -272,7 +201,7 @@ function Dodo() {
                             <Input placeholder="输入验证码" />
                         </Col>
                         <Col span={12}>
-                            <Button type='primary'>发送验证码</Button>
+                            <Button type='primary' onClick={() => sendAuthCode(serverId)}>发送验证码</Button>
                         </Col>
                     </Row>
                 </Form.Item>
@@ -280,6 +209,201 @@ function Dodo() {
         </Modal>
     </>
     );
+}
+
+function DoDoActivityCreateModal(props: {
+    isModalShow: boolean,
+    setIsModalShow: (isShow: boolean) => void,
+}) {
+    const { isModalShow, setIsModalShow } = props;
+    const social_tool = 'dodo';
+
+    const [pushServerId, setPushServerId] = useState("");
+    const [currServerChannels, setCurrServerChannels] = useState<BotChannel[]>([]);
+    const [currServerRoles, setCurrServerRoles] = useState<BotRoles[]>([]);
+    const [botServers, setBotServers] = useState<BotServer[]>([]);
+    const [activities, setActivities] = useState<ActivityItem[]>([]);
+    const [activityCount, setActivityCount] = useState(0);
+    const [page, setPage] = useState(1);
+    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+    const [form] = Form.useForm();
+
+    const handleCancel = () => {
+        setIsModalShow(false);
+    };
+
+    const activityColumns: ColumnsType<ActivityItem> = [
+        {
+            title: '互动/藏品ID',
+            dataIndex: 'activity_id',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: '类型',
+            dataIndex: 'activity_type',
+        },
+        {
+            title: '名称',
+            dataIndex: 'name',
+        },
+        {
+            title: '合约地址',
+            dataIndex: 'contract.contract_address',
+            render:(text: string, record: ActivityItem) => short(record.contract?.contract_address as string),
+        },
+        {
+            title: '开始时间',
+            dataIndex: 'start_time',
+            render: (num: number) => num ? formatDate(new Date(num * 1000)) : '',
+        },
+        {
+            title: '结束时间',
+            dataIndex: 'end_time',
+            render: (num: number) => num ? formatDate(new Date(num * 1000)) : '',
+        },
+    ]
+
+    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
+
+    const createServerActivity = async (values: any) => {
+        if (selectedRowKeys.length === 0) {
+            message.error('请选择活动');
+            return;
+        }
+
+        const activity_id = selectedRowKeys[0];
+
+        const { 
+            content,
+            color_theme = 'blue',
+            channel_id,
+            role_id,
+        } = values;
+        
+        const meta = {
+            activity_id,
+            channel_id,
+            color_theme,
+            content,
+            roles: [
+                role_id,
+            ]
+        }
+
+        const serverMeta = botServers.find(server => server.raw_server_id === pushServerId);
+        if (!serverMeta) {
+            message.error('请选择服务器');
+            return;
+        }
+
+        const res = await createPushInfo(serverMeta.id.toString(), meta);
+
+        await pushBotActivity(await res.id);
+        
+        setIsModalShow(false);
+    }
+    
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+        type: 'radio',
+    };
+
+    useEffect(() => {
+        if (!isModalShow) return;
+        getBotServers(social_tool, 1, 1000).then(res => {
+            setBotServers(res.items);
+        });
+    }, [isModalShow]);
+
+    useEffect(() => {
+        if (!isModalShow) return;
+        getActivities({page: 1, limit: 10}).then(res => {
+            setActivities(res.items);
+            setActivityCount(res.count);
+        });
+    }, [isModalShow]);
+
+    useEffect(() => {
+        if (!pushServerId) return;
+        getServerChannels(pushServerId, social_tool).then(res => {
+            setCurrServerChannels(res);
+        });
+        getServerRoles(pushServerId, social_tool).then(res => {
+            setCurrServerRoles(res);
+        });
+    }, [pushServerId]);
+
+    return (<>
+        <Modal 
+            title="推送设置" 
+            open={isModalShow}
+            onOk={() => form.submit()} 
+            onCancel={handleCancel} 
+            width={800}
+            okText={"确定"}
+            cancelText={"取消"}
+        >
+            <Alert type='info' message='可以将活动推送到授权的 DoDo 或 Discord 社区频道中，提供给社群成员进行藏品铸造，活动需先在管理后台中创建好'/>
+            <Form className='mt-20' form={form} onFinish={createServerActivity}>
+                <Row>
+                    <Col span={11}>
+                        <Form.Item label="推送超级群" name="server_id">
+                            <Select onChange={val => setPushServerId(val)}>
+                                {botServers.map(server => <Option key={server.id} value={server.raw_server_id}>{server.server_name}</Option>)}
+                            </Select>
+                        </Form.Item>
+                    </Col>
+                    <Col span={11} offset={1}>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={11}>
+                        <Form.Item label="推送频道" name="channel_id">
+                            <Select>
+                                {currServerChannels.map(channel => <Option key={channel.channelId} value={channel.channelId}>{channel.channelName}</Option>)}
+                            </Select>
+                        </Form.Item>
+                    </Col>
+                    <Col span={11} offset={1}>
+                        <Form.Item label="身份组" name="role_id">
+                            <Select>
+                                {currServerRoles.map(channel => <Option key={channel.roleId} value={channel.roleId}>{channel.roleName}</Option>)}
+                            </Select>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Divider orientation="left">设置推送内容</Divider>
+                <Row>
+                    <Col span={11}>
+                        <Form.Item label="消息内容" name="content">
+                            <Input placeholder="推送消息内容" />
+                        </Form.Item>
+                    </Col>
+                    <Col span={11}>
+                        {/* 颜色主题 */}
+                    </Col>
+                </Row>
+                <Divider orientation='left'>选择推送消息</Divider>
+                <Row>
+                    <Table 
+                        columns={activityColumns} 
+                        dataSource={activities} 
+                        rowSelection={rowSelection} 
+                        rowKey="id"
+                        pagination={{
+                            total: activityCount,
+                            current: page,
+                            showTotal: (total) => `共 ${total} 条`,
+                        }}
+                        onChange={(info: TablePaginationConfig) => setPage(info.current as number)}
+                    />
+                </Row>
+            </Form>
+        </Modal>
+    </>);
 }
 
 function Discord() {
