ce405c86506fa95022f327a8e37c6d4ce2177e0c
PanaW <jiuhuadao@proton.me>
2023-06-09 17:08:55 +0800

    public charge

--- src/services/pay.ts
+++ src/services/pay.ts
@@ -1,4 +1,4 @@
-import { get, post } from '.';
+import { get, post, put } from '.';
 
 export async function createWxPayOrder (metadata: any) {
     return await post('/dashboard/pay/wxOrder', metadata);
@@ -6,4 +6,20 @@ export async function createWxPayOrder (metadata: any) {
 
 export async function getWxPayOrderDetail(id: number) {
     return await get(`/dashboard/pay/wxOrder/${id}`);
+}
+
+export async function getCmbCardNo() {
+    return await get('/dashboard/users/cmbDepositNo');
+}
+
+export async function createCmbCardNo(name: string, card_no: string, bank: string) {
+    return await post('/dashboard/users/cmbDepositNo', {name, card_no, bank});
+}
+
+export async function updateCmbCardRelation(name: string, card_no: string, bank: string) {
+    return await put('/dashboard/users/cmbDepositNo', {name, card_no, bank});
+}
+
+export async function getCmbDeposits() {
+    return await get('/dashboard/users/cmbRecentDeposits');
 }
